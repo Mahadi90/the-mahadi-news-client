987bc1d0547a39e1aab01a9c7393a04a6c5614ba
Mahadi Hasan <mahadimhs787@gmail.com>
2023-12-03 23:35:25 +0600

    navbar added

--- src/pages/shared/header/Header.jsx
+++ src/pages/shared/header/Header.jsx
@@ -1,17 +1,47 @@
-import React from 'react';
-import logo from '../../../assets/logo2.jpeg'
-import moment from 'moment';
+import React from "react";
+import logo from "../../../assets/logo2.jpeg";
+import moment from "moment";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Marquee from "react-fast-marquee";
+import profile from '../../../assets/user.png'
 
 const Header = () => {
-    return (
-        <div>
-          <div className='mx-auto text-center'>
-          <img src={logo} alt="" />
-           <p className='text-secondary'>Journalism Without Fear or Favor</p>
-           <h4 className='text-secondary'>{moment().format('dddd,MMMM D,YYYY')}</h4>
-          </div>
-        </div>
-    );
+  return (
+    <Container>
+      <div className="mx-auto text-center">
+        <img src={logo} alt="" />
+        <p className="text-secondary mt-2">Journalism Without Fear or Favor</p>
+        <h4 className="text-secondary">
+          {moment().format("dddd,MMMM D,YYYY")}
+        </h4>
+      </div>
+      <div className="d-flex bg-secondary p-2">
+        <Button variant="danger">Latest</Button>
+        <Marquee className="text-white" speed={100} pauseOnHover={true}>
+          The Mahadi news is very special.It is provide more and more important
+          new that you need.Daily hot news.
+        </Marquee>
+      </div>
+      <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">The Mahadi News</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mx-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">About</Nav.Link>
+            <Nav.Link href="#link">Career</Nav.Link>
+          </Nav>
+          <Nav className="d-flex align-items-center">
+            <img className="me-2" style={{width : 40}} src={profile} alt="" />
+            <Nav.Link className="bg-dark text-white px-5 fw-bold" href="#link">Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </Container>
+  );
 };
 
 export default Header;
